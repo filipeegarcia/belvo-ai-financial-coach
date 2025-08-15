c9d4b11c4640e3a965ec0335bdc449ad48b0067d
Filipe Garcia <filipe.garcia1997@gmail.com>
2025-08-14 23:12:12 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure for /belvo subpath when proxied
-  basePath: '/belvo',
-  assetPrefix: '/belvo',
-  
   // Environment variables
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
