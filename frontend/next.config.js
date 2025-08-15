0bbc83d1ccb45c72fb6729b31d86781142d6c73c
Filipe Garcia <filipe.garcia1997@gmail.com>
2025-08-14 23:20:48 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Environment variables
+  basePath: '/belvo',
+  trailingSlash: false,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_ENVIRONMENT: process.env.NODE_ENV,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
