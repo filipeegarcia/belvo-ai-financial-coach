9de6e598f9944b8566261b1882245d2a2314a4e4
Filipe Garcia <filipe.garcia1997@gmail.com>
2025-08-14 23:23:26 -0300

    trigger ci

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/belvo',
-  trailingSlash: false,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_ENVIRONMENT: process.env.NODE_ENV,
   },
 };
-
 module.exports = nextConfig;
